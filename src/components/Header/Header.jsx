8fa9c48a49adfe28d7518f801e63408fc4357f0a
Serhii-Zamorylo23 <129538617+Serhii-Zamorylo23@users.noreply.github.com>
2025-07-29 20:47:41 +0300

    New branch header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import logo from './logo1.svg'
+import userIcon from "./user1.svg"
+import { IoIosArrowDown } from "react-icons/io";
+import {useState} from "react";
+const Header = () => {
+    const [isOpen,setIsOpen]=useState(false);
+    const handleClick=()=>{
+        setIsOpen(!isOpen)
+    }
+    return(
+        <>
+            <header className= "header">
+                <div className="header_wrapper">
+                    <img src={logo} alt="logo" className="header_wrapper_logo" />
+                    <nav className="header_wrapper_nav">
+                        <a href="#">Who we are</a>
+                        <a href="#">Contacts</a>
+                        <a href="#">Menu</a>
+                    </nav>
+                    <div className="header_wrapper_userContainer">
+                        <button className="header_wrapper_userContainer_button">Sign Up</button>
+                        <img className="header_wrapper_userContainer_avatar" src={userIcon} alt="userIcon"/>
+                        <p onClick={handleClick} className="header_wrapper_userContainer_burgerMunuTitle" >Menu</p>
+                        <IoIosArrowDown size={14} className="header_wrapper_userContainer_burgerMunuArrow" />
+                    </div>
+                </div>
+            </header>
+            <section className="mobileHeader" style={{ display: isOpen ? 'flex' : 'none'}}>
+                <div className="mobileHeader_wrapper">
+                    <nav className="mobileHeader_wrapper_nav">
+                        <a href="#">Who we are</a>
+                        <a href="#">Contacts</a>
+                        <a href="#">Menu</a>
+                    </nav>
+                    <div className="mobileHeader_wrapper_userContainer">
+                        <div className="mobileHeader_wrapperr_userContainer_avatar">
+                            <img className="mobileHeader_wrapper_userContainer_avatar" src={userIcon} alt=""/>
+                        </div>
+                        <button className="mobileHeader_wrapper_userContainer_button">Sign Up</button>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+
+export default Header;
