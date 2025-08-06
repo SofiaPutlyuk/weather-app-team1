1fad770e342ad895b5121aecd1bf9b668de0664d
Serhii-Zamorylo23 <zamorylo@i.ua>
2025-08-06 18:52:15 +0300

    new API

--- src/components/News_/News.jsx
+++ src/components/News_/News.jsx
@@ -2,20 +2,20 @@ import React, { useEffect, useState } from "react";
 import NewsItem from "./NewsItem";
 
 const News = () => {
-  const key = "bb13245f736a46919ce5d5ff12b77344";
+  const key = "23c75b719bf7daa612c390e942d17d29";
   const [article, setArticle] = useState([]);
   useEffect(() => {
-    fetch(`https://newsapi.org/v2/everything?q=Pets&from=2025-08-06&sortBy=popularity&apiKey=${key}`)
+    fetch(`https://gnews.io/api/v4/search?q=Pets&apikey=${key}`)
       .then((response) => response.json())
       .then((data) => setArticle(data.articles));
-  }, [article]);
+  }, []);
   return (
     <section className="NewsSection">
       <div className="NewsSection_wrapper">
         <h3 className="NewsSection_wrapper_name">Interacting with our pets</h3>
         <div className="NewsSection_wrapper_news">
           {Array.isArray(article)&&article.map((articl, index) => (
-            <NewsItem key={index} img={articl.urlToImage} title={articl.title}/>
+            <NewsItem key={index} img={articl.image} title={articl.title} url={articl.url}/>
           ))}
         </div>
         <button className="NewsSection_wrapper_button">See more</button>
