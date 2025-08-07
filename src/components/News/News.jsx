ce24a78fbf4993200f1fdab673fba1e221322b91
Serhii-Zamorylo23 <zamorylo@i.ua>
2025-08-07 18:50:05 +0300

    functionoal button seeMore

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -4,11 +4,15 @@ import NewsItem from "./NewsItem";
 const News = () => {
   const key = "23c75b719bf7daa612c390e942d17d29";
   const [article, setArticle] = useState([]);
+  const [item,setItem] =useState(4)
   useEffect(() => {
-    fetch(`https://gnews.io/api/v4/search?q=Pets&apikey=${key}`)
+    fetch(`https://gnews.io/api/v4/search?q=Pets&apikey=${key}&max=${item} `)
       .then((response) => response.json())
       .then((data) => setArticle(data.articles));
-  }, []);
+  }, [item]);
+  const moreItem=()=>{
+    setItem(item + 4)
+  }
   return (
     <section className="NewsSection">
       <div className="NewsSection_wrapper">
@@ -18,7 +22,7 @@ const News = () => {
             <NewsItem key={index} img={articl.image} title={articl.title} url={articl.url}/>
           ))}
         </div>
-        <button className="NewsSection_wrapper_button">See more</button>
+        <button className="NewsSection_wrapper_button" onClick={moreItem}>See more</button>
       </div>
     </section>
   );
